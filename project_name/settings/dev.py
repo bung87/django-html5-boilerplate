2f03f36b2d5680f111dcb1039e06e7388cc1185b
bung <zh.bung@gmail.com>
2014-06-30 18:32:08 +0800

    new try

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -40,3 +40,45 @@ STATIC_URL = '/static/'
 # Overwrite default ROOT_URLCONF to include static file serving by Django.
 # In production, this should be handled separately by your webserver or CDN.
 ROOT_URLCONF = '{{ project_name }}.urls.dev'
+
+CDN_FINDER_DIR = os.path.join(PROJECT_ROOT, 'assets')
+CDN_FINDER_PREFIX ='http://libs.useso.com/'
+
+INSTALLED_APPS +=('cdn_finder',)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+       'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'cdn_finder.templatetags.cdn_finder_tags': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+    }
+}
